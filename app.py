76af00b3f437cf772048c110053001497790f69b
juan jose <juanjoseocoromunoz@gmail.com>
2023-05-18 17:16:48 -0500

    confirmacion3

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def almacen():
 def admin_index():
     return render_template('admin/index.html')
 
+@app.route('/admin/login')
+def admin_login():
+    return render_template('admin/login.html')
+
 if __name__ =='__main__':
     app.run(debug=True)
