d71bca15e8d1ce387587da3781ff2d2bb956cca1
juan jose <juanjoseocoromunoz@gmail.com>
2023-05-18 14:41:03 -0500

    confirmacion2

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app=Flask(__name__)
 def inicio():
     return render_template('sitio/index.html')
 
+@app.route('/almacen')
+def almacen():
+    return render_template('sitio/almacen.html')
+
+
 if __name__ =='__main__':
     app.run(debug=True)
