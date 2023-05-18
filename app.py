0f3fbafc140687a52251d30529df9b0cea472381
juan jose <juanjoseocoromunoz@gmail.com>
2023-05-18 15:07:33 -0500

    confirmacion3

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ def inicio():
 def almacen():
     return render_template('sitio/almacen.html')
 
+@app.route('/admin')
+def admin_index():
+    return render_template('admin/index.html')
 
 if __name__ =='__main__':
     app.run(debug=True)
